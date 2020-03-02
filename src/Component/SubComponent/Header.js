e5095a38583f7758308d0f75d4ca7d4d6c544c49
unknown <aj9930061@gmail.com>
2020-03-01 23:46:39 -0800

    abc

--- src/Component/SubComponent/Header.js
+++ src/Component/SubComponent/Header.js
@@ -1,9 +1,13 @@
-import React, { Component,Fragment } from 'react';
+import React, { Component,Fragment, useState } from 'react';
+import About from '../About';
 // import logoImage from '../../../public/images/pic.png';
 
 
-export default class Header extends Component {
-    render() {
+export default function Header() {
+    const [tabs, setTabs] = useState('home');
+    const handleTabs = (tabValue) => {
+        setTabs(tabValue);
+    }
         return(
             <Fragment>
                 <div className="block-45">
@@ -26,43 +30,36 @@ export default class Header extends Component {
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" />
                     </button>
-                    <div className="collapse navbar-collapse navbar-light" id="navbarsExample05">
-                        <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active" style={{marginRight:'50px'}}>
-                            <a className="nav-link" href="/">Home</a>
-                        </li>
-                        <li className="nav-item dropdown" style={{marginRight:'50px'}}>
-                            <a className="nav-link" href="About" id="dropdown04" >About</a>
-                            {/* <div className="dropdown-menu" aria-labelledby="dropdown04">
-                            <a className="dropdown-item" href="introduction">Introduction</a>
-                            <a className="dropdown-item" href="AboutAnilDimple">Ms.Dimple Anil</a>
-                            
-                            </div> */}
-                        </li>
-                        <li className="nav-item dropdown" style={{marginRight:'50px'}}>
-                            <a className="nav-link dropdown-toggle" href="sermon.html" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Prayers</a>
-                            <div className="dropdown-menu" aria-labelledby="dropdown05">
-                            <a className="dropdown-item" href="DailyPrayer">Daily Prayer</a>
-                            <a className="dropdown-item" href="Direction">Direction</a>
-                            <a className="dropdown-item" href="OBES">Obe's</a>
-                            
-                            </div>
-                        </li>
-                        <li className="nav-item" style={{marginRight:'50px'}}>
-                            <a className="nav-link" href="Miracle">Miracles</a>
-                        </li>
-                        <li className="nav-item" style={{marginRight:'50px'}}>
-                            <a className="nav-link" href="Blog">Blogs</a>
-                        </li>
-                        <li className="nav-item" style={{marginRight:'50px'}}>
-                            <a className="nav-link" href="Contact">Contact</a>
-                        </li>
-                        </ul>
-                    </div>
+                        <div className="collapse navbar-collapse navbar-light" id="navbarsExample05">
+                            <ul className="navbar-nav ml-auto">
+                            <li className={tabs === 'home' ? "nav-item active": 'nav-item'} style={{marginRight:'50px'}} >
+                                <a className="nav-link" href="/">Home</a>
+                            </li>
+                            <li className={tabs === 'about' ? "nav-item active": 'nav-item'} style={{marginRight:'50px'}} >
+                                <a className="nav-link" href="About">About  </a>
+                            </li>
+                            <li className= {tabs === 'prayers' ? "nav-item dropdown active": 'nav-item dropdown'}  style={{marginRight:'50px'}}>
+                                <a className="nav-link dropdown-toggle" href="sermon.html" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Prayers</a>
+                                <div className="dropdown-menu" aria-labelledby="dropdown05">
+                                    <a className="dropdown-item" href="/DailyPrayer">Daily Prayer</a>
+                                    <a className="dropdown-item" href="/Direction">Direction</a>
+                                    <a className="dropdown-item" href="/OBES">Obe's</a>
+                                </div>
+                            </li>
+                            <li className={tabs === 'miracles' ? "nav-item active": 'nav-item'}  style={{marginRight:'50px'}}>
+                                <a className="nav-link" href="/Miracle">Miracles</a>
+                            </li>
+                            <li className={tabs === 'blogs' ? "nav-item active": 'nav-item'}  style={{marginRight:'50px'}}>
+                                <a className="nav-link" href="/Blog">Blogs</a>
+                            </li>
+                            <li className={tabs === 'contact' ? "nav-item active": 'nav-item'}  style={{marginRight:'50px'}}>
+                                <a className="nav-link" href="/Contact">Contact</a>
+                            </li>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
                 </header>   
             </Fragment> 
         )
-    }
 }
