46caeb37d401428a3aaad6a623b8dc9fbef4ead8
unknown <aj9930061@gmail.com>
2020-03-02 05:32:52 -0800

    changes

--- src/Component/SubComponent/Miracles.js
+++ src/Component/SubComponent/Miracles.js
@@ -28,22 +28,127 @@ export default function Miracles (){
     setDialogContent(contentArray[objectIndex].content);
     setShowContentDialog(true);
    }
-        return(
-              <div>
-      <section className="site-section bg-light">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="section-heading">
-              <h2 className="heading">Miracles</h2>
-            </div>
+  return(
+      <div>
+        <section className="site-section bg-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="section-heading">
+                  <h2 className="heading">Miracles</h2>
+                </div>
+              </div>
+            </div>      
           </div>
-        </div>      
-      </div>
-      <div className="block-13" style={{marginLeft:'90px'}}>
-        <div className="nonloop-block-13 owl-carousel owl-loaded owl-drag">
-          <div className="owl-stage-outer">
-            <div className="owl-stage" style={{transform: 'translate3d(0px, 0px, 0px)', transition: 'all 0.25s ease 0s', width: '2540px', paddingLeft: '150px', paddingRight: '150px'}}>
+               <div className="block-13" style={{marginLeft:'90px'}}>
+                 <div className="nonloop-block-13 owl-carousel owl-loaded owl-drag">
+                    <div className="owl-stage-outer">
+                      <section class="site-section"><div class="container"><div class="row">
+                        <div class="col-md-6 col-lg-4 mb-5">1
+                          <div class="block-20">
+                            <figure style={{width: '100%'}}>
+                              <a href="blog-single.html">
+                                 <img src="images/pic3.jpeg" alt="Image placeholder" class="img-fluid"/>
+                              </a>
+                            </figure>
+                         <div class="text text-center">
+                            <h3 class="heading">
+                              <a href="#">I come from and got married into a joint family.</a>
+                            </h3>
+                             <div class="meta mb-3">
+                                <div>
+                                   <a href="#">
+                                     <span class="fa fa-calendar"></span> May 29, 2018</a>
+                                </div>
+                                 <div>
+                                   <a href="#">
+                                     <span class="fa fa-user"></span> Admin</a>
+                                  </div>
+                                   <div>
+                                     <a href="#">
+                                      <span class="fa fa-comment"></span> 19</a>
+                                      </div>
+                                      </div>
+                                      <p style={{textalign: 'justify'}}>I come from a joint family and got married into a joint family. In my new family, I saw that each one loved to...</p>
+                                      <p>
+                                        <a href="" data-toggle="modal" data-target="#exampleModalLong">Read More</a>
+                                        </p>
+                                        </div>
+                                        </div>
+                                        </div>
+                                        <div class="col-md-6 col-lg-4 mb-5">
+                                          <div class="block-20">
+                                            <figure style={{width: '100%'}}>
+                                              <a href="blog-single.html">
+                                                <img src="images/believe.jpeg" alt="Image placeholder" class="img-fluid"/>
+                                                </a>
+                                                </figure>
+                                                <div class="text text-center">
+                                                  <h3 class="heading">
+                                                    <a href="#">I come from and got married into a joint family.</a>
+                                                    </h3>
+                                                    <div class="meta mb-3">
+                                                      <div>
+                                                        <a href="#">
+                                                          <span class="fa fa-calendar"></span> May 29, 2018</a>
+                                                          </div>
+                                                          <div>
+                                                            <a href="#">
+                                                              <span class="fa fa-user"></span> Admin</a>
+                                                              </div>
+                                                              <div>
+                                                                <a href="#">
+                                                                  <span class="fa fa-comment"></span> 19</a>
+                                                                  </div>
+                                                                  </div>
+                                                                  <p style={{textalign: 'justify'}}>Life seems to be sorted fully.Thank you Dimple MA.Love you.I wanted a clean, spacious home.I have.I wanted to ...</p>
+                                                                  <p>
+                                                                    <a href="" data-toggle="modal" data-target="#exampleModalLong">Read More</a>
+                                                                    </p>
+                                                                    </div>
+                                                                    </div>
+                                                                    </div>
+                                                                    <div class="col-md-6 col-lg-4 mb-5">
+                                                                      <div class="block-20">
+                                                                        <figure style={{width: '100%'}}>
+                                                                          <a href="blog-single.html">
+                                                                            <img src="images/baby.jpeg" alt="Image placeholder" class="img-fluid"/>
+                                                                            </a>
+                                                                            </figure>
+                                                                            <div class="text text-center">
+                                                                              <h3 class="heading">
+                                                                                <a href="#">I come from and got married into a joint family.</a>
+                                                                                </h3>
+                                                                                <div class="meta mb-3">
+                                                                                  <div>
+                                                                                    <a href="#">
+                                                                                      <span class="fa fa-calendar"></span> May 29, 2018</a>
+                                                                                      </div>
+                                                                                      <div>
+                                                                                        <a href="#">
+                                                                                          <span class="fa fa-user"></span> Admin</a>
+                                                                                          </div>
+                                                                                          <div>
+                                                                                            <a href="#"><span class="fa fa-comment"></span> 19</a>
+                                                                                            </div>
+                                                                                            </div>
+                                                                                            <p style={{textalign: 'justify'}}>Please share I conceived after four years of marriage, loads and loads of efforts. I was happy but insecure an...</p>
+                                                                                            <p>
+                                                                                              <a href="" data-toggle="modal" data-target="#exampleModalLong">Read More</a>
+                                                                                              </p>
+                                                                                              </div>
+                                                                                              </div>
+                                                                                              </div>
+                                                                                              
+                                                                                               
+                                                                                               
+                                                                                                    
+                                                                                                      
+                                                                                                     
+            </div>
+            </div>
+            </section>
+            {/* <div className="owl-stage" style={{transform: 'translate3d(0px, 0px, 0px)', transition: 'all 0.25s ease 0s', width: '2540px', paddingLeft: '150px', paddingRight: '150px'}}>
               {contentArray.map((data, index) => {
                 return(
                   <div className="owl-item active" style={{width: '353.333px', marginRight: '20px'}}>
@@ -63,7 +168,7 @@ export default function Miracles (){
                 )
               })}
                             
-            </div>
+            </div> */}
           </div>  
         </div>
       </div>
